79250c301e55ba116d8cefa139ec65f06fa957b8
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-24 13:18:47 -0500

    Diferencia

--- Preinforme12_SantiagoDelValle_B.go
+++ Preinforme12_SantiagoDelValle_B.go
@@ -4,22 +4,25 @@
 //Punto numero 4 "la cantidad de semanas consecutivas en las que la presión promedio semanal supera o está por debajo del promedio anual de presiones semanales."
 //Punto numero 6 "Temperaturas"
 //punto numero 6.1 "Desviaciones"
+//Punto numero 6.2 "Clasificación"
+//punto numero 6.3 "Promedio de desviaciones"
 
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
 
-	ListaGrados := []float64{200.783333, 196.111, 197.583333, 197.66667, 198.738889, 200.883333, 200.405556, 219.288889, 219.227778, 213.255556, 216.011111, 220.538889, 216.927778, 219.588889, 214.444, 216.111, 217.778, 215.494444, 215.538889, 199.966667, 201.538889, 201.677778, 195.461111, 199.194444, 193.077778, 199.38889, 193.933333, 191.05, 200.883333, 191.688889, 197.427778, 195.877778, 198.5, 197.383333, 199.811111, 225.605556, 229.338889, 232.377778, 222.566667222, 222.94444, 225.305556, 224.788889, 229.494444, 227.172222, 231.961111, 222.66667, 225.066667, 191.566667, 201.994444, 196.155556, 192.244444, 196.394444}
+	Desviaciongeneral := 13.08
+	DesviacionB := 5.37
+	DesviacionM := 3.32
 
-	sort.Float64s(ListaGrados)
+	promedio := (DesviacionB + DesviacionM) / 2.0
 
-	fmt.Println(ListaGrados)
+	fmt.Println("El promedio de las desviaciones fue de", promedio, "F°")
 
-	fmt.Println(len(ListaGrados))
+	Diferencia := Desviaciongeneral - promedio
+
+	fmt.Println("La diferencia entre el la desviación general y el promedio entre la buena y la mala es de", Diferencia)
 
 }
